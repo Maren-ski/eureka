a055b4c339616adf9c40e70590dab66737f48af2
Maren-ski <62648982+Maren-ski@users.noreply.github.com>
2020-03-27 10:42:37 +0100

    UserList App
    First basic components for App that shows list of users and let's you add new users

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header/>
+    <UserList v-bind:users= "users"/>
+
+  </div>
+</template>
+
+<script>
+import UserList from './components/UserList.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserList,
+    Header
+  },
+  data() {
+    return {
+      users: [
+        {userId: "1", userName: "John", feesPaid: false},
+        {userId: "2", userName: "Mary", feesPaid: true},
+        {userId: "3", userName: "Salma", feesPaid: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p> {{ user.userName }} </p>
+
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+    name: "Users",
+    props: ["user"]
+  }
+</script>
